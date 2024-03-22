c5e80a10deef916bfe701299ed5f271f3c6ef166
Billy <asmasawi@gmail.com>
2024-03-22 16:27:09 +0000

    added the otion to save passwords to a file

--- passgenerator.sh
+++ passgenerator.sh
@@ -15,8 +15,19 @@ if ! [[ $PASS_LENGTH =~ ^[0-9]+$ ]]; then
     exit 1
 fi
 
-# loop will create 5 passwords according to user as per length given by user
-for p in $(seq 1 5);
+# loop will create 3 passwords according to user as per length given by user
+for p in $(seq 1 3);
 do
     openssl rand -base64 48 | cut -c1-$PASS_LENGTH
 done
+
+# Ask user if they want to save the passwords to a file
+echo "Do you want to save these passwords to a file? (y/n)"
+read  choice
+if [ "$choice" = "y" ]; then
+    echo "Saving passwords to passwords.txt"
+    for p in $(seq 1 3); do
+       openssl rand -base64 48 | cut -c1-$PASS_LENGTH >>passwords.txt
+    done
+    echo "Passwords saved to passwords.txt"
+fi
