fa7fb9f291bbf50ce856985efd607d732c1c3590
Billy <asmasawi@gmail.com>
2024-03-22 18:08:03 +0000

    added an array and created file

--- passgenerator.sh
+++ passgenerator.sh
@@ -14,20 +14,26 @@ if ! [[ $PASS_LENGTH =~ ^[0-9]+$ ]]; then
     echo "Error: Please enter a valid number."
     exit 1
 fi
+#Creating an array named passwords to capture generated passwords 
+passwords=()
 
-# loop will create 3 passwords according to user as per length given by user
+# loop will create 3 passwords according to user as per length given by user and save them to array
 for p in $(seq 1 3);
 do
-    openssl rand -base64 48 | cut -c1-$PASS_LENGTH
+    passwords+=("$(openssl rand -base64 48 | cut -c1-$PASS_LENGTH )")
 done
+
+#Display generated passwords
+echo "Here are the generated passwords:"
+printf "%s\n" "${passwords[@]}"
 
 # Ask user if they want to save the passwords to a file
 echo "Do you want to save these passwords to a file? (y/n)"
 read  choice
 if [ "$choice" = "y" ]; then
-    echo "Saving passwords to passwords.txt"
-    for p in $(seq 1 3); do
-       openssl rand -base64 48 | cut -c1-$PASS_LENGTH >>passwords.txt
-    done
     echo "Passwords saved to passwords.txt"
+       printf "%s\n" "${passwords[@]}" >>passwords.txt
+
+elif [ "$choice" = "n" ]; then
+     echo "Passwords not saved."
 fi
